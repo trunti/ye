cf94559a6610769bb240627fa94eeaa82bd491a6
trunti <andrigud15@ru.is>
2015-11-27 15:04:30 +0000

    class

---
+++ group.h
@@ -0,0 +1,14 @@
+#ifndef GROUP_H
+#define GROUP_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Group
+{
+public:
+    Group();
+};
+
+#endif // GROUP_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "group.h"
 using namespace std;
 int main()
 {
